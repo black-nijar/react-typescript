7b65d74e926cb0cc0bf6dbbd7f98c54e4424d072
Nijarudeen  Rajak <50949038+black-nijar@users.noreply.github.com>
2021-07-29 14:27:22 +0530

    done to understand basic

---
+++ src/PeopleList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { PeopleState as Props } from "./App";
+
+interface PeopleProps {
+  people: Props["people"];
+}
+
+export const PeopleList: React.FC<PeopleProps> = ({ people }) => {
+  const renderList = (): JSX.Element[] => {
+    return people.map((person) => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.img} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
+};
